09f49efd46ae61297819e5e8f4128c4708b4869c
Cucurrucho <adam.bar91@gmail.com>
2025-02-20 13:17:21 -0300

    full installation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,22 +2,24 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider {
+class AppServiceProvider extends ServiceProvider
+{
     /**
      * Register any application services.
      */
-    public function register(): void {
+    public function register(): void
+    {
         //
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void {
-        Schema::defaultStringLength(191);
-        //
+    public function boot(): void
+    {
+        Vite::prefetch(concurrency: 3);
     }
 }
